e02e3d210e8a303121cd2abf7b0049329907ff13
mipo17 <michael-poelzl@outlook.com>
2020-11-26 08:31:19 +0100

    update canvas -integration bug

--- packages/client/src/tasks/fill-shape-task/index.ts
+++ packages/client/src/tasks/fill-shape-task/index.ts
@@ -1,26 +1,31 @@
 import viewHtml from "./view.html";
+import { Task } from "../../task";
 
-export class FillShapeTask extends HTMLElement {
+export default class FillShapeTask extends Task {
   loadButton: HTMLAnchorElement;
   serverResponseSpan: HTMLSpanElement;
+  counter: number = 0;
   canvasElement: HTMLCanvasElement;
-  async connectedCallback() {
+  async onMounted() {
     this.attachShadow({ mode: "open" });
     this.shadowRoot.innerHTML = viewHtml;
-    this.loadButton = this.shadowRoot.getElementById("load-button_1") as HTMLAnchorElement;
-    this.serverResponseSpan = this.shadowRoot.getElementById("fill-shape") as HTMLSpanElement;
+    this.loadButton = this.shadowRoot.getElementById("load-button") as HTMLAnchorElement;
+    this.serverResponseSpan = this.shadowRoot.getElementById("fill-shape-task") as HTMLSpanElement;
     this.loadButton.onclick = this.loadFromServer.bind(this);
-
     this.canvasElement = this.shadowRoot.getElementById("myCanvas") as HTMLCanvasElement;
     var ctx = this.canvasElement.getContext("2d");
     ctx.beginPath();
-    ctx.arc(50, 50, 40, 0, 2 * Math.PI);
+    ctx.arc(200, 200, 80, 0, 2 * Math.PI);
     ctx.stroke();
-
-    
-
   }
+
+  onUnmounting(): void | Promise<void> {}
+
   async loadFromServer() {
+    this.counter++;
+    if (this.counter > 5) {
+      this.finish(true);
+    }
     this.loadButton.innerText = "loading ...";
     try {
       const response = await fetch("/api/test");
