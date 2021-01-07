2404d2e7dfec735d759b98d981eb7b915f9cf0c9
mipo17 <michael96.poelzl@gmail.com>
2021-01-07 12:27:07 +0100

    -added statsStorage

--- packages/client/src/statsStorage.ts
+++ packages/client/src/statsStorage.ts
@@ -1,14 +1,34 @@
-import { TaskIdentifier } from "./taskManager";
+import { min } from "lodash";
 
+export type taskName = "fill-shape-task" | "drag-and-drop-task";
 export class StatsStorage {
-  taskCompleted(taskName: TaskIdentifier, timeInMS: number) {
-    // TODO
-  }
+  private tasks: Array<[taskName, number]> = [];
+  private localStorageKey = "stats";
 
-  getStats(): any {
-    return {
-      "fill-shape-task": 15,
-      "drag-and-drop-task": 222,
-    };
+  taskCompleted(taskName: taskName, timeInMS: number) {
+    //check if task already in array:
+    var index = 0;
+    var flagFound = false;
+    for (index = 0; index < this.tasks.length; index++) {
+      if (this.tasks[index][0] == taskName) {
+        flagFound = true;
+        break;
+      }
+    }
+    if (flagFound) {
+      this.tasks[index][1] = min([timeInMS, this.tasks[index][1]]);
+    } else {
+      this.tasks.push([taskName, timeInMS]);
+    }
+    this.store(this.tasks);
+  }
+  private store(tasks: Array<[taskName, number]>) {
+    localStorage.setItem(this.localStorageKey, JSON.stringify(tasks));
+  }
+  getStats(): Array<[taskName, number]> {
+    return JSON.parse(localStorage.getItem(this.localStorageKey));
+  }
+  deleteStats() {
+    localStorage.removeItem(this.localStorageKey);
   }
 }
