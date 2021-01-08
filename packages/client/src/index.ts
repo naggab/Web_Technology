d24aaefb23f66038776024cdf1af3481d112d9cb
Gabriel Nagler <naglergabriel.gn@gmail.com>
2021-01-08 15:46:04 +0100

    Bug fixed

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -1,33 +1,32 @@
 import "./components/button";
 import "./components/container";
 
-import {router} from "./router";
-import {MasterOfDisaster} from "./masterOfDisaster";
+import { router } from "./router";
+import { MasterOfDisaster } from "./masterOfDisaster";
 import "./components/taskOpener";
-import {TaskOpener} from "./components/taskOpener";
+import { TaskOpener } from "./components/taskOpener";
 import "./components/slider_switch/index";
-import {Button} from "./components/button";
+import { Button } from "./components/button";
 
 function main() {
-    document.body.style.backgroundImage = "/assets/img/bg.png";
-    let _showAllTasks = document.querySelector("#show-all-tasks");
-    _showAllTasks.onclick = showAllTasks;
+  document.body.style.backgroundImage = "/assets/img/bg.png";
+  let _showAllTasks: Button = document.querySelector("#show-all-tasks");
+  _showAllTasks.onclick = showAllTasks;
 }
-async function showAllTasks(){
+async function showAllTasks() {
   router("all-tasks");
 }
 window.addEventListener("load", () => {
-    main();
+  main();
 });
 
 document.addEventListener("DOMContentLoaded", () => {
-    document.body.addEventListener("click", (e) => {
-        const target = e.target as HTMLAnchorElement;
-
-    });
-    MasterOfDisaster.setup().then(() => {
-        (<any>window).MOD = MasterOfDisaster.getInstance();
-        document.body.appendChild(new TaskOpener());
-        router("welcome-start");
-    });
+  document.body.addEventListener("click", (e) => {
+    const target = e.target as HTMLAnchorElement;
+  });
+  MasterOfDisaster.setup().then(() => {
+    (<any>window).MOD = MasterOfDisaster.getInstance();
+    document.body.appendChild(new TaskOpener());
+    router("welcome-start");
+  });
 });
