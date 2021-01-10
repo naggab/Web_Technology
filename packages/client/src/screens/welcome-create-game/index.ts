f1bbc6faaa238aeb5fe37b35592867f01c04d743
Gabriel Nagler <naglergabriel.gn@gmail.com>
2021-01-10 12:40:25 +0100

    Bug fixed

--- packages/client/src/screens/welcome-create-game/index.ts
+++ packages/client/src/screens/welcome-create-game/index.ts
@@ -7,7 +7,7 @@ import { MasterOfDisaster } from "../../masterOfDisaster";
 import { CommandOp } from "@apirush/common/src";
 
 class WelcomeCreateGame extends AbstractScreen {
-  _inputBox: any;
+  _gameName_input: any;
   _createButton: Button;
   _mod: MasterOfDisaster;
 
@@ -18,16 +18,16 @@ class WelcomeCreateGame extends AbstractScreen {
 
   onMounted() {
     this._mod = MasterOfDisaster.getInstance();
-    this._inputBox = this.shadowRoot.querySelector("text-box");
+    this._gameName_input = this.shadowRoot.querySelector("text-box");
 
     this._createButton = this.shadowRoot.querySelector("#create-game-button");
-    this._inputBox = this._inputBox.shadowRoot.querySelector("input").value;
 
     this._createButton.onclick = this.createGame.bind(this);
   }
 
   async createGame() {
-    await this._mod.createGame(this._inputBox);
+    await this._mod.createGame(this._gameName_input.getValue()
+  );
   }
 
   async getHtml() {
