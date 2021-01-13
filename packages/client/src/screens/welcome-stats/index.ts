0a6adcbd36e284585ef9b428091feb1c7038b1cd
Lukas Brugger <lukas.brugger404@gmail.com>
2021-01-13 11:39:46 +0100

    Stat base

--- packages/client/src/screens/welcome-stats/index.ts
+++ packages/client/src/screens/welcome-stats/index.ts
@@ -1,10 +1,26 @@
 import templateHTML from "./template.html";
 import AbstractScreen from "../AbstractScreen";
+import "../../components/textBox";
+import { MasterOfDisaster } from "../../masterOfDisaster";
+import { Button } from "../../components/button";
+import { StatsStorage } from "../../statsStorage";
+
+
 
 class StatsScreen extends AbstractScreen {
+  _userName_input: any;
+  _mod: MasterOfDisaster;
+
+
   constructor() {
     super();
     document.title = "Stats";
+  }
+
+  onMounted(){
+    this._userName_input = this.shadowRoot.querySelector("#userName");
+    this._userName_input.setHint(this._mod.getString().welcome_start.userName);
+
   }
 
   async getHtml() {
